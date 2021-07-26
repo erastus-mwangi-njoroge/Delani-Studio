59e4e45422267d5e2e0ec6a6c1f9fccd95419c34
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-07-26 17:17:46 +0300

    add a jquery code to show and hide information in what we do section

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".design").click(function() {
+      $("#hidden").toggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".development").click(function() {
+      $("#hidden1").toggle("slow");
+    });
+});
+$(document).ready(function(){
+    $(".product").click(function() {
+      $("#hidden2").toggle("slow");
+    });
+});
